d8a40444aadd348cbd332399ce6003730f8e775b
KTheAsianimeBoi <minhkhoahc2002@gmail.com>
2023-12-22 14:18:14 +0900

    First commit

--- dev_program_DB/dev_program_DB/mydb/runManager.bat
+++
@@ -1,2 +0,0 @@
-rem Copyright(C) 2007 National Institute of Informatics, All rights reserved.
-java -classpath ..\lib\hsqldb.jar org.hsqldb.util.DatabaseManager -url jdbc:hsqldb:hsql://localhost/hotel_entities
